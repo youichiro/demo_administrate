4a467e3b6e74681b2aa8fcf28c55c6c77d09dedd
youichiro <cinnamon416@gmail.com>
2022-06-26 12:55:00 +0900

    Revert "bin/rails db:migrate"
    This reverts commit 1fab44a0ec3fc5c5fbe8aca3e97f2a443690cead.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_26_034656) do
-  create_table "categories", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_categories_on_post_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_06_26_030909) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,5 +19,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_26_034656) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "categories", "posts"
 end
