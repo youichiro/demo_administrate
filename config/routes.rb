c5b28503e7a86b9d4960da54225e0b05384a140f
youichiro <cinnamon416@gmail.com>
2022-06-26 13:00:55 +0900

    add admin/tags routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :tags
+
   namespace :admin do
       resources :posts
+      resources :tags
 
       root to: "posts#index"
     end
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
